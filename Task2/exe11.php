e167a1733cab386c39f7f35f70dc9f0973f42a11
kyawzawthu <kyawzawthu00@gmail.com>
2020-10-15 21:37:30 +0630

    final assignment

---
+++ Task2/exe11.php
@@ -0,0 +1,23 @@
+<?php 
+
+$a=1000;
+$ba=1200;
+$c=1400;
+$d="$";
+
+echo  "<table border=1>";
+	echo "<tr>";
+		echo "<td>Salary of Mr.A is</td>";
+		echo "<td>$a$d</td>";
+	echo "</tr>";
+	echo "<tr>";
+		echo "<td>Salary of Mr.B is</td>";
+		echo "<td>$ba$d</td>";
+	echo "</tr>";
+		echo "<tr>";
+		echo "<td>Salary of Mr.C is</td>";
+		echo "<td>$c$d</td>";
+	echo "</tr>";
+echo "</table>";
+
+?>
